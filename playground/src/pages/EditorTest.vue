c5790d501ca732b2182429ed40cfa44499e78c06
vanilla-note <143585697+vanilla-note@users.noreply.github.com>
2025-03-25 17:00:49 +0900

    Create EditorTest.vue

---
+++ playground/src/pages/EditorTest.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>Editor Test Page</h1>
+      <VanillanoteTest />
+    </div>
+</template>
+
+<script setup lang="ts">
+import VanillanoteTest from '../components/VanillanoteTest.vue';
+</script>
