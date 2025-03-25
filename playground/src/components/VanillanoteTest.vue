6aa4f0412af8ba34091d6678109878dbb7191bce
vanilla-note <143585697+vanilla-note@users.noreply.github.com>
2025-03-25 17:00:32 +0900

    Create VanillanoteTest.vue

---
+++ playground/src/components/VanillanoteTest.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div data-vanillanote data-note-id="test-editor"></div>
+      <button @click="initEditor">Create Editor</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+/*
+import { onMounted } from 'vue';
+// 👉 npm link 후 import 가능
+import { getVanillanote, createVanillanote, destroyVanillanote } from 'vanillanote';
+
+const initEditor = () => {
+const vn = getVanillanote();
+    createVanillanote(vn);
+};
+
+onMounted(() => {
+// Playground 최초 로딩 시 자동 생성 원하면 여기에
+});
+*/
+</script>
