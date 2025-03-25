b7c634849c8654d405bc1adc6390834f774ead7c
vanilla-note <143585697+vanilla-note@users.noreply.github.com>
2025-03-25 17:00:07 +0900

    Create App.vue

---
+++ playground/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/editor-test">Editor Test</router-link>
+      </nav>
+      <router-view />
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+nav {
+margin-bottom: 20px;
+}
+nav a {
+margin-right: 10px;
+}
+</style>
