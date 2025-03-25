00b909e908da2481f90e7ae139e61988466c5c57
vanilla-note <143585697+vanilla-note@users.noreply.github.com>
2025-03-25 17:00:59 +0900

    Create Home.vue

---
+++ playground/src/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+      <h1>Welcome to Vanillanote Playground</h1>
+      <p>Click the menu to test the editor.</p>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
