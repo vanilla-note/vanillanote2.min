807ad8a178331d963a26b4f1d9aa2a376f0bd6f7
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-26 16:57:33 +0900

    Create events.ts

---
+++ src/types/events.ts
@@ -0,0 +1,6 @@
+export interface DocumentEvents {
+	selectionchange: null | ((e: any) => void);
+	keydown: null | ((e: any) => void);
+	resize: null | ((e: any) => void);
+	resizeViewport: null | ((e: any) => void);
+}
