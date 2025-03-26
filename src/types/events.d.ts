b73b2b650db22e17959a443fe59b7ef5d3c21bcc
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-26 16:56:39 +0900

    Delete src/types/events.d.ts

--- src/types/events.d.ts
+++
@@ -1,6 +0,0 @@
-export interface documentEvents {
-	selectionchange: null | ((e: any) => void);
-	keydown: null | ((e: any) => void);
-	resize: null | ((e: any) => void);
-	resizeViewport: null | ((e: any) => void);
-}
