77f21b341c2545f401a62e86863e67808a372925
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-28 17:02:07 +0900

    Update main.ts

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,4 +2,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+import { getVanillanote, getVanillanoteConfig } from 'vanillanote2';
+import type { Vanillanote } from 'vanillanote2';
+
+const app = createApp(App);
+
+// 1. Vanillanote 객체 생성
+const vnConfig = getVanillanoteConfig();
+const vn: Vanillanote = getVanillanote(vnConfig);
+
+// 2. 전역 속성으로 등록
+app.config.globalProperties.$vn = vn;
+
+// 3. 앱 마운트
+app.use(router).mount('#app');
