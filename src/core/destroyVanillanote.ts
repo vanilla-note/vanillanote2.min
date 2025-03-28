e5b906cc5c352c11d6555a377a69dc8eaabce97c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-28 16:56:54 +0900

    Update destroyVanillanote.ts

--- src/core/destroyVanillanote.ts
+++ src/core/destroyVanillanote.ts
@@ -1,5 +1,8 @@
 import type { Vanillanote } from "../types/vanillanote";
 
-export const destroyVanillanote = (vn: Vanillanote, element: HTMLElement) => {
+export const destroyVanillanote = (vn: Vanillanote, element?: HTMLElement) => {
+    const targetElement = element ? element : document;
 
+    //if there is no note, no create.
+    const notes = targetElement.querySelectorAll('[data-vanillanote]');
 }
