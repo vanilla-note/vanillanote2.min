15a10102972ecf13171513de38f5e289e250fc4c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-28 17:00:37 +0900

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export type { Consts, ClassNames } from "./types/consts";
+export type { Csses, Colors } from "./types/csses";
+export type { ToolPositions } from "./types/enums";
+export type { DocumentEvents } from "./types/events";
+export type { LanguagePack, LanguageSet } from "./types/language";
+export type { Vanillanote, VanillanoteConfig, VanillanoteElement } from "./types/vanillanote";
+export type { Variables } from "./types/variables";
+
+export { getVanillanote, getVanillanoteConfig } from "./core/getVanillanote";
