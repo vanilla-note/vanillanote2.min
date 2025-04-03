1d41336b07f2840905a02a75c27ccb26e802a6a1
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-03 15:02:44 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export type { Csses, Colors } from "./types/csses";
 export type { ToolPosition } from "./types/enums";
 export type { DocumentEvents } from "./types/events";
 export type { LanguagePack, LanguageSet } from "./types/language";
-export type { Vanillanote, VanillanoteConfig, VanillanoteElement } from "./types/vanillanote";
+export type { Vanillanote, VanillanoteConfig, VanillanoteElement, NoteData, NoteFileData, NoteImageData, NoteLinkData, NoteVideoData } from "./types/vanillanote";
 export type { Variables } from "./types/variables";
 
 export { getVanillanote, getVanillanoteConfig } from "./core/getVanillanote";
